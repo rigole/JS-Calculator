94e2ab0b00dd994c032f3e0fd76bfb933383e33c
rigole <foplacide@gmail.com>
2021-08-23 20:50:46 +0100

    adding the decimal functionality

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ function sendNumberValue(number) {
     calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number
 }
 
+function addDecimal() {
+   // If no decimal, add one
+    if (!calculatorDisplay.textContent.includes('.')){
+        calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`
+    }
+}
+
 // Add Event Listeners ofr numbers, operators and decimal buttons
 inputBtns.forEach((inputBtn) => {
     if (inputBtn.classList.length === 0 ) {
@@ -16,7 +23,7 @@ inputBtns.forEach((inputBtn) => {
     } else if (inputBtn.classList.contains('operator')){
         inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value))
     } else if (inputBtn.classList.contains('decimal')){
-        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value))
+        inputBtn.addEventListener('click', () => addDecimal())
     }
 })
 
